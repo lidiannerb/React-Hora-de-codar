04b87aa161d6a6a9726cce2e982d822b83557270
Lidianne Barbosa <lidiannerb@gmail.com>
2022-06-19 12:11:37 -0300

    feat:condicional rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import "./App.css";
-import Evento from './components/Evento';
-import Form from "./components/Form";
+import Condicional from './components/Condicional';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Testando evento</h1>
-      <Evento />
-      <Form />
+      <h1>Renderização condicional</h1>
+      <Condicional />
     </div>
   );
 }
